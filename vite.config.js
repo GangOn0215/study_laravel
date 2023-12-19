0112d1c71e9d8f60e986542416eedd5c0caea7ba
GangOn0215 <96044518+GangOn0215@users.noreply.github.com>
2023-12-19 21:59:11 +0900

    setting update
    셋팅부분 업데이트 된것들

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,16 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/js/app.js',
+                'resources/js/tailwind.js',
+                'resources/js/layout.js',
+
                 'resources/css/app.css',
                 'resources/css/common.css',
                 'resources/css/layout.css',
                 'resources/css/project/project.css',
                 'resources/css/project/todo.css',
                 'resources/css/lib/jquery-ui.css',
-                'resources/js/app.js',
-                'resources/js/tailwind.js',
-                'resources/js/lib/jquery-ui.js',
-                'resources/js/lib/jquery-3.7.1.js',
-                'resources/js/layout.js',
             ],
             refresh: true,
         }),
@@ -24,7 +23,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 5173,
         hmr: {
-            host: 'localhost',
+            host: '13.209.20.195',
         },
     },
 });
